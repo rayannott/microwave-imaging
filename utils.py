6779a9777b0f9e694100dfee7b0bf34a7f3fa3e3
rayannott <imjobjob@gmail.com>
2022-11-19 15:44:23 +0100

    added to utils, changed trans.ipynb

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
-import os
 
-def abs_path_from_filename(filename):
-    return os.path.abspath(os.path.join("data", "dummy", f"{filename}.img"))
+def abs_path_from_filename(dummy_or_radar: str, filename: str):
+    import os
+    assert dummy_or_radar in ["dummy", "radar"], f"Unknown data folder: {dummy_or_radar}"
+    return os.path.abspath(os.path.join("data", dummy_or_radar, f"{filename}.img"))
+
+def load_labels():
+    import json
+    with open('data/dummy_labels.json') as f:
+        dummy = json.load(f)
+    with open('data/radar_labels.json') as f:
+        radar = json.load(f)
+    return dummy, radar
+        
+def output_stats(data):
+    import numpy as np
+    
+    print('mean and std\n\t', np.mean(data, axis=0), np.std(data, axis=0))
+    print('max and min\n\t', np.max(data, axis=0), np.min(data, axis=0))
